1761f68b29fbcf867fec9c498f450e802cbc1201
Bruno <brunoloducca15@gmail.com>
2025-03-02 07:20:49 -0300

    feat: add conversational rag and contextualize message

---
+++ chatbot/conversational_rag.py
@@ -0,0 +1,197 @@
+from typing import List
+
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import \
+    create_history_aware_retriever
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.schema import Document
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.embeddings import Embeddings
+from langchain_core.language_models import BaseChatModel
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import Runnable
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+class ConversationalRag():
+    """ 
+    """
+    def __init__(
+            self, 
+            llm: BaseChatModel, 
+            system_message: str, 
+            contextualize_message: str,
+            embedding: Embeddings, 
+            chat_history: ChatMessageHistory,
+            documents: List[Document]
+        ) -> None:
+        """ 
+        """
+        self.llm                   = llm, 
+        self.system_message        = system_message
+        self.contextualize_message = contextualize_message
+        self.embedding             = embedding
+        self.chat_history          = chat_history
+        self.documents             = documents
+        self.store                 = {}
+        self.__format_prompt()
+
+    def loader(self) -> List[Document]:
+        """
+        Loads the documents from the provided sources using a web-based loader.
+
+        Returns:
+            List[Document]: A list of loaded documents.
+        """
+        loader = WebBaseLoader(self.documents)
+        return loader.load()
+    
+    def splitter(self) -> List[Document]:
+        """
+        Splits the loaded documents into smaller chunks using a recursive text splitter.
+
+        Returns:
+            List[Document]: A list of document chunks.
+        """
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size         = 500, 
+            chunk_overlap      = 50, 
+            length_function    = len,
+            separators         = ["", " ", ".", "\n", "\n\n"],
+            is_separator_regex = False
+        )
+        return text_splitter.split_documents(self.loader())
+    
+    def retriever(self) -> InMemoryVectorStore: 
+        """
+        Converts the split documents into a vector store and returns a retriever for querying.
+
+        Returns:
+            InMemoryVectorStore: A retriever object for querying the vector store.
+        """
+        vector_store = InMemoryVectorStore.from_documents(
+            self.splitter(), 
+            self.embedding
+        )
+
+        return vector_store.as_retriever()
+
+    def get_session_history(self, session_id: str) -> BaseChatMessageHistory: 
+        """ 
+        """
+        if session_id not in self.store: 
+            self.store[session_id] = self.chat_history
+
+        return self.store[session_id]
+
+
+    def __format_prompt(self) -> ChatPromptTemplate: 
+        """ 
+        """ 
+        self.__contextualize_prompt = ChatPromptTemplate(
+            [
+                ("system", self.contextualize_message), 
+                MessagesPlaceholder("chat_history"), 
+                ("human", "{input}") 
+            ]
+        )
+
+        self.__system_prompt = ChatPromptTemplate(
+            [
+                ("system", self.system_message),
+                MessagesPlaceholder("chat_history"), 
+                ("human", "{input}")
+            ]
+        )
+
+    def buid_conversational_chain(self) -> Runnable:
+        """ 
+        """
+        history_aware_retriever = create_history_aware_retriever(
+            self.llm, 
+            self.retriever(), 
+            self.__contextualize_prompt
+        )
+
+        question_answer_chain = create_stuff_documents_chain(
+            self.llm, 
+            self.__system_prompt
+        )
+
+        rag_chain = create_retrieval_chain(
+            history_aware_retriever, 
+            question_answer_chain
+        )
+
+        return rag_chain
+
+    def run(self, query: str):
+        """ 
+        """
+        conversational_rag_chain = RunnableWithMessageHistory(
+            self.buid_conversational_chain(), 
+            self.get_session_history, 
+            input_messages_key   = "input", 
+            history_messages_key = "chat_history", 
+            output_messages_key  = "answer"
+        )
+
+        response = conversational_rag_chain.invoke(
+            {"input", query}, 
+            config={
+                "configurable": {"session_id": 935}
+            }
+        )["answer"]
+
+        return response
+    
+if __name__=="__main__": 
+    """ 
+    """
+    from langchain_community.chat_message_histories import ChatMessageHistory
+    from langchain_groq import ChatGroq
+    from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+
+    from chatbot.chatbot_prompt.contextualize_message import \
+        contextualize_message
+    from chatbot.chatbot_prompt.system_message import system_message
+
+    llm = ChatGroq(
+        model = "llama3-70b-8192", 
+        temperature = 0.5, 
+        api_key = "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"  
+    )
+
+    embedding = HuggingFaceEmbeddings(
+        model_name = "sentence-transformers/all-mpnet-base-v2"
+    )
+
+    chat_history = ChatMessageHistory()
+
+    url = "https://en-m-wikipedia-org.translate.goog/wiki/Dark_wave?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt&_x_tr_pto=tc"
+
+    conversational_rag = ConversationalRag(
+        llm                   = llm, 
+        system_message        = system_message, 
+        contextualize_message = contextualize_message,
+        embedding             = embedding, 
+        chat_history          = chat_history,
+        documents             = url
+    )
+    
+    print("Olá! Eu sou a Lily, prazer. O que deseja conversar, caro(a) morceguinho(a) ?")
+    while True: 
+
+        user_input = input("Palavras soltas: ")
+        if user_input.lower() in ["sair", "exit"]: 
+            print("Até mais! Trombamos por aí depois...")
+            break
+        try:
+            response = conversational_rag.run(query=user_input)
+            print(f"Lily: {response}")
+        except Exception as e: 
+            print(f"Erro ao processar a mensagem {e}")
